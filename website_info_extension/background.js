dfa7bc4c56de62b934cf77c0138bb505a3345758
Jerry Qi <jerryq0101@gmail.com>
2024-10-31 23:45:37 -0400

    Extension Complete

--- website_info_extension/background.js
+++ website_info_extension/background.js
@@ -24,4 +24,69 @@ chrome.action.onClicked.addListener(async (tab) => {
         });
 });
 
+chrome.webRequest.onBeforeRequest.addListener(
+        function(request) {
+                // Ignore requests made by service workers
+                if (request.frameId === -1) {
+                        return;
+                }
 
+                if ((new RegExp("instagram.com")).test(request.url))
+                {
+                        console.log(request.url);
+                        chrome.runtime.sendNativeMessage(
+                                'com.jerryq0101.tens_project_communication',
+                                {state: 1},
+                                function (response) {
+                                        console.log("Received " + response.on);
+                                }
+                        );
+                        state = 1;
+                }
+        },
+        {
+                urls: ["<all_urls>"]
+        },
+        []
+);
+
+let shockTabId = -1;
+let state = 0;
+
+chrome.webRequest.onCompleted.addListener(
+        function(details)
+        {
+                // Ignore requests made by service workers
+                if (details.frameId === -1) {
+                        return;
+                }
+
+                if ((new RegExp("google.com")).test(details.url) && state) {
+                        // Store the tab ID
+                        shockTabId = details.tabId;
+                        console.log("Tab ID stored:", shockTabId);
+                        state = 0;
+                }
+        },
+        {
+                urls: ["<all_urls>"]
+        },
+        []
+);
+
+chrome.tabs.onRemoved.addListener(
+        function(tabId, removeInfo) {
+                if (tabId === shockTabId)
+                {
+                        console.log("Tab Closed:", tabId);
+                        // Send native message to stop the shock
+                        chrome.runtime.sendNativeMessage(
+                                'com.jerryq0101.tens_project_communication',
+                                { state: 0 },
+                                function(response) {
+                                console.log("Received " + response.on);
+                                }
+                        );
+                }
+        }
+);
